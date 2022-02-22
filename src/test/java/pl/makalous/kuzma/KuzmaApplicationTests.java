973830b48757af5b8172a7b6a56c1105555ee74a
makalous <mak.ajwen@gmail.com>
2022-02-22 18:33:06 +0100

    first commit

---
+++ src/test/java/pl/makalous/kuzma/KuzmaApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.makalous.kuzma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KuzmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
