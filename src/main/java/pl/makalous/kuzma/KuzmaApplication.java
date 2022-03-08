c3b0adb4960eab4fea24d13b67a47fc32db3454d
makalous <mak.ajwen@gmail.com>
2022-03-08 14:16:11 +0100

    electricsearch + acturator + java11 upgrade

--- src/main/java/pl/makalous/kuzma/KuzmaApplication.java
+++ src/main/java/pl/makalous/kuzma/KuzmaApplication.java
@@ -2,13 +2,12 @@ package pl.makalous.kuzma;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication
 public class KuzmaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KuzmaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(KuzmaApplication.class, args);
+    }
 
 }
--- src/main/java/pl/makalous/kuzma/config/KuzmaConfig.java
+++ src/main/java/pl/makalous/kuzma/config/KuzmaConfig.java
@@ -2,10 +2,14 @@ package pl.makalous.kuzma.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import pl.makalous.kuzma.entity.KuzmaEntity;
 
 @Configuration
-public class KuzmaConfig {
+@EnableWebSecurity
+public class KuzmaConfig extends WebSecurityConfigurerAdapter {
     @Bean
     KuzmaEntity kuzmaEntity() {
         return new KuzmaEntity();
@@ -15,4 +19,12 @@ public class KuzmaConfig {
     io.spring.guides.gs_producing_web_service.Kuzma kuzmaSoap() {
         return new io.spring.guides.gs_producing_web_service.Kuzma();
     }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring()
+                .antMatchers("/**");
+    }
+
 }
