669832ee384aef5ed570c43447341ff50b86d13c
Makser <mak.ajwen@gmail.com>
2023-08-25 12:53:51 +0200

    simple M(V)C for microservice

---
+++ src/main/java/pl/makalous/mvc/model/Team.java
@@ -0,0 +1,17 @@
+package pl.makalous.mvc.model;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Builder
+@Getter
+@Setter
+@ToString
+public class Team {
+    private int id;
+    private String name;
+    private String colour;
+    private int howManyPlayers;
+}
---
+++ src/main/java/pl/makalous/mvc/service/TeamService.java
@@ -0,0 +1,31 @@
+package pl.makalous.mvc.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pl.makalous.mvc.model.Team;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class TeamService {
+    private List<Team> teams;
+
+    @Autowired
+    TeamService(List<Team> teams) {
+        this.teams = teams;
+    }
+
+    public List<Team> getById(int id) {
+        if (teams.size() < 1) return List.of(Team.builder().build());
+        return teams.stream().filter(p -> p.getId() == id).collect(Collectors.toList());
+    }
+
+    public void setTeam(Team e) {
+        teams.add(e);
+    }
+
+    public void updateName(int id, String name) {
+        teams.stream().filter(p->p.equals(getById(id).get(0))).findAny().get().setName(name);
+    }
+}
