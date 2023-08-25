e5cb953bf76ffe6d8f44c7c96767d884fbeebf0b
Makser <mak.ajwen@gmail.com>
2023-08-25 17:13:37 +0200

    strategy ehhh tbh looks like factory (not the same, similar)

---
+++ src/main/java/pl/makalous/strategy/EvenBiggerThanLastOne.java
@@ -0,0 +1,10 @@
+package pl.makalous.strategy;
+
+public class EvenBiggerThanLastOne {
+    SomethingBiggerLol somethingBiggerLol = new SomethingBiggerLol();
+
+    void doSth() {
+        somethingBiggerLol.setShipping(new InpostShipping(123123123));
+    }
+
+}
---
+++ src/main/java/pl/makalous/strategy/Shipping.java
@@ -0,0 +1,5 @@
+package pl.makalous.strategy;
+
+public interface Shipping {
+    String ship(String address);
+}
